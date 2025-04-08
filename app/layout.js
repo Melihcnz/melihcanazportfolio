51adeda42ca8afaf08293ef8d20f1855f36cafe8
Melih Canaz <mcanaz1234@gmail.com>
2025-04-08 22:38:33 +0300

    first commit

---
+++ app/layout.js
@@ -0,0 +1,22 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+export const metadata = {
+  title: "Melih Canaz - Full Stack Developer",
+  description: "Full Stack Developer & Yazılım Uzmanı",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="tr">
+      <body className={`${inter.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
