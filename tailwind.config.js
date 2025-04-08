4306c380e4c09f5fa20b5f49475b67ab80074b7e
Melih Canaz <mcanaz1234@gmail.com>
2025-04-08 22:41:59 +0300

    Fix Tailwind and PostCSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,28 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
         muted: 'var(--muted)',
         accent: 'var(--accent)',
-        primary: 'var(--primary)',
         card: 'var(--card)',
+        'card-foreground': 'var(--card-foreground)',
+        border: 'var(--border)',
+        ring: 'var(--ring)',
+        primary: 'var(--primary)',
+        'primary-foreground': 'var(--primary-foreground)',
       },
     },
   },
